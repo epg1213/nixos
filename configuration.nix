db1d13b7eaf0abeb0210dd7ba4f61bf537c5a39e
epg1213 <eliot.glarner@ikmail.com>
2026-08-26 17:58:27 +0200

    hyprland base config

---
+++ configuration.nix
@@ -0,0 +1,75 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+  nixpkgs.config.allowUnfree = true;
+  networking.hostName = "blueteeth"; # Define your hostname.
+
+  networking.wireless.iwd = {
+    enable = true;
+    settings = {
+      Network.EnableIPv6 = false;
+      Settings.AutoConnect = true;
+    };
+  };
+
+  services.displayManager = {
+    gdm.enable = true;
+    defaultSession = "hyprland";
+  };
+
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.epg1213 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.bash;
+    home = "/home/epg1213";
+    packages = with pkgs; [
+      firefox
+      neovim
+      discord
+    ];
+    initialPassword = "changeme";
+  };
+
+  # List packages installed in system profile.
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    ghostty
+    kitty
+  ];
+
+  # Locales
+  time.timeZone = "Europe/Paris";
+  i18n.defaultLocale = "fr_FR.UTF-8";
+  console.keyMap = "fr";
+  services.xserver.xkb.layout = "fr";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true;
+
+}
+
