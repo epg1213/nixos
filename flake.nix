7691a458f8119c7663ee3fcc4fe3eaf1d0290c39
epg1213 <eliot.glarner@ikmail.com>
2026-08-27 18:42:14 +0200

    home-manager + refactor to list hosts dir automagically

--- flake.nix
+++ flake.nix
@@ -4,17 +4,42 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-26.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.blueteeth = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [./hosts/blueteeth];
-    };
-    
-    nixosConfigurations.blueeye = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [./hosts/blueeye];
-    };
+  outputs = { self, nixpkgs, ... }@inputs:
+  let
+    lib = inputs.nixpkgs.lib;
+    hosts = builtins.filter (x: x != null) (
+      lib.mapAttrsToList (name: value: if (value == "directory") then name else null) (
+        builtins.readDir ./hosts
+    ));
+  in
+  {
+    nixosConfigurations = builtins.listToAttrs (
+      map (
+        host: {
+          name = host;
+          value = lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [
+	      { config.networking.hostName = host; }
+	      (./hosts + "/${host}")
+              inputs.home-manager.nixosModules.default {
+                home-manager = {
+                  useGlobalPkgs = true;
+                  useUserPackages = true;
+                  extraSpecialArgs = { inherit inputs; };
+                  users.epg1213 = ./home/epg1213;
+                };
+	      }
+	    ];
+	  };
+	}
+      ) hosts
+    );
   };
 }
